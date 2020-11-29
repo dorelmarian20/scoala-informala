ebaa18eeb4083ac4912e7774b57c35bfdb5c7cf1
dorelmarian20 <dorelmariansandor@gmail.com>
2020-11-29 13:29:20 +0200

    final  (exercise 7 is not done)

--- TemaJS/exercitiu2.js
+++
@@ -1,9 +0,0 @@
-function compare (a, b) {
-    if(a < b) {
-        return -1;  
-    }else if (a === b) {
-        return 0;
-    }else if (a > b) {
-        return 1;
-    }
-}
--- TemaJS/exercitiu3.js
+++
@@ -1,8 +0,0 @@
-function sumaPrime (num) {
-  let sum = 0;
-  for(var i=1; i<=num; i++){
-    sum += i;
-  }
-  return sum * 4 / 2 - 2;
-}
-console.log(sumaPrime(3) );
