faabb2b95d2c1e051648e8c2b7c50886eafd6f41
dorelmarian20 <dorelmariansandor@gmail.com>
2021-03-13 16:38:07 +0200

    react forms

--- proiectfinal/src/features/sandbox/Sandbox.jsx
+++ proiectfinal/src/features/sandbox/Sandbox.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Button } from "semantic-ui-react";
-import { decrement, increment } from "./testReducer";
+import { increment, decrement } from "../sandbox/testReducer";
+import { openModal } from "../../app/common/modals/modalReducer";
 
 export default function Sandbox() {
   const dispatch = useDispatch();
@@ -20,6 +21,13 @@ export default function Sandbox() {
         onClick={() => dispatch(decrement(10))}
         content="Decrement"
         color="red"
+      />
+      <Button
+        onClick={() =>
+          dispatch(openModal({ modalType: "TestModal", modalProps: { data } }))
+        }
+        content="Open Modal"
+        color="teal"
       />
     </>
   );
